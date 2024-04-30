4f1da83dd79fe5b7ba06cbf8db0f5f1f255e38b0
kauavidal <kaua.magalhaes@sptech.school>
2024-04-30 17:55:36 -0300

    vf

--- script.sh
+++ script.sh
@@ -5,6 +5,27 @@ echo "Atualizando o sistema operacional"
 sudo apt update && sudo apt upgrade
 echo "---------------------------------------------------------------"
 
+echo "Verificando se o NodeJS está instalado"
+node -v #verifica versao atual do java
+if [ $? = 0 ]; #se retorno for igual a 0
+        then #entao,
+                echo "Node instalado" #print no terminal
+		echo "Prosseguindo para proxima etapa ..."
+		echo "---------------------------------------------------------------"
+
+        else #se nao,
+                echo "NodeJS não instalado" #print no terminal
+                echo "gostaria de instalar o NodeJS ? [s/n]" #print no terminal
+
+                read get #variável que guarda resposta do usuário
+
+        if [ \"$get\" == \"s\" ]; #se retorno for igual a s
+                then #entao
+		curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+                sudo apt-get install -y nodejs #executa instalacao do nodejs
+        fi #fecha o 2º if
+fi #fecha o 1º if
+
 echo "Verificando se o java está instalado"
 java --version #verifica versao atual do java
 if [ $? = 0 ]; #se retorno for igual a 0
@@ -22,26 +43,6 @@ if [ $? = 0 ]; #se retorno for igual a 0
 		then #entao
 		sudo apt install openjdk-17-jre -y #executa instalacao do java
 	fi #fecha o 2º if
-fi #fecha o 1º if
-
-echo "Verificando se o NodeJS está instalado"
-node -v #verifica versao atual do java
-if [ $? = 0 ]; #se retorno for igual a 0
-        then #entao,
-                echo "Node instalado" #print no terminal
-		echo "Prosseguindo para proxima etapa ..."
-		echo "---------------------------------------------------------------"
-
-        else #se nao,
-                echo "NodeJS não instalado" #print no terminal
-                echo "gostaria de instalar o NodeJS ? [s/n]" #print no terminal
-
-                read get #variável que guarda resposta do usuário
-
-        if [ \"$get\" == \"s\" ]; #se retorno for igual a s
-                then #entao
-		sudo apt install nodejs #executa instalacao do nodejs
-        fi #fecha o 2º if
 fi #fecha o 1º if
 
 echo "Verificando se o banco de dados MySQL está instalado"
